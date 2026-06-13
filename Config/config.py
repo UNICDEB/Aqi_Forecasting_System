bcda534b43ca1663a98b3a8659559370832c8306
UNICDEB <doloidebabrat99@gmail.com>
2026-06-13 19:09:24 +0530

    Update Version 1.0.0.4 (Add Config File)

---
+++ Config/config.py
@@ -0,0 +1,19 @@
+# config/config.py
+
+INPUT_WINDOW = 60
+
+OUTPUT_WINDOW = 60
+
+FEATURES = [
+    "PM10",
+    "PM25",
+    "PM1",
+    "NO2",
+    "SO2",
+    "CO",
+    "O3",
+    "CO2",
+    "TEMP",
+    "HUMIDITY",
+    "AQI"
+]
